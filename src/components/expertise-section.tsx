f83022890e032992f9751ddcadd2596ab6b35053
Divyansh <divyanshverma18may@gmail.com>
2025-03-23 16:56:50 +0530

    expertise

--- src/components/expertise-section.tsx
+++ src/components/expertise-section.tsx
@@ -3,16 +3,16 @@ import React from "react";
 
 const ExpertiseSection = () => {
   return (
-    <div className="bg-[#F7F7FA] h-[700px] w-full max-sm:px-4 flex flex-col items-center justify-center">
-      <div className="max-w-[1100px] mx-auto">
+    <div className="bg-[#F7F7FA] h-[700px] w-full max-sm:px-4 flex flex-col items-center justify-center ">
+      <div className="max-w-5xl mx-auto px-4 sm:px-10">
         <div className="relative w-full flex h-[300px] items-center">
-          <div className="w-full mb-auto">
-            <h1 className="font-bold text-4xl max-w-[600px]">
-              Let&apos;s start new journey with me, I&apos;ve{" "}
-              <span className="text-[#4BB543]">expertise</span> in these
-              software&apos;s
+          <div className="w-full mb-auto flex flex-col gap-5">
+            <h1 className="font-bold text-3xl w-full">
+              Skilled in industry-leading software,
+              <br /> I have <span className="text-[#4BB543]">expertise</span> in
+              these tools.
             </h1>
-            <p className="text-sm mt-[27px] text-[#333333] w-full tracking-wider leading-[36px]">
+            <p className="text-sm text-[#333333] w-full tracking-wider leading-[28px]">
               From creating wireframes and prototypes to high-fidelity mockups
               and animations, I blend design tools and fashion insights to bring
               my creations to life.
@@ -22,12 +22,12 @@ const ExpertiseSection = () => {
             <Image src={"/person-thinking.svg"} fill alt="expertise" />
           </div>
         </div>
-        <div className="w-full grid grid-cols-3 gap-12">
+        <div className="w-full flex items-center justify-between gap-10">
           <div className="flex flex-col gap-4">
             <Image
               src={"/figma.svg"}
-              width={45}
-              height={45}
+              width={36}
+              height={36}
               alt="figma"
               className="h-1/2"
             />
@@ -40,8 +40,8 @@ const ExpertiseSection = () => {
           <div className="flex flex-col gap-4">
             <Image
               src={"/adobe-suite.svg"}
-              width={60}
-              height={60}
+              width={64}
+              height={64}
               alt="figma"
               className="h-1/2"
             />
@@ -53,15 +53,15 @@ const ExpertiseSection = () => {
           </div>
           <div className="flex flex-col gap-4">
             <Image
-              src={"/procreate.svg"}
-              width={80}
-              height={80}
+              src={"/miro.svg"}
+              width={60}
+              height={60}
               alt="figma"
               className="h-1/2"
             />
-            <h1 className="font-bold text-xl">Procreate</h1>
+            <h1 className="font-bold text-xl">Miro</h1>
             <p className="text-black/80">
-              I am an expert in using Procreate. Knowledge of CLO-3D as well.
+              I am an expert in using Miro. Knowledge of JIRA as well.
             </p>
           </div>
         </div>
