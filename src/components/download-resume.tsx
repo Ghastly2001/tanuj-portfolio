3641c534a9a3c9d6a6c5fefbfcabc24bc2862eda
Divyansh <divyanshverma18may@gmail.com>
2025-03-23 18:03:43 +0530

    landing complete

--- src/components/download-resume.tsx
+++ src/components/download-resume.tsx
@@ -10,7 +10,7 @@ const DownloadResume = () => {
   };
   return (
     <div
-      className="fixed w-[52px] h-[165px] bg-[#4BB543] rounded-l-md right-0 top-1/3"
+      className="fixed w-[52px] h-[165px] bg-[#4BB543] rounded-l-md right-0 top-1/3 z-[9999]"
       onClick={handleDownloadResume}
     >
       <p className="absolute -rotate-90 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white  font-bold text-xl flex items-center justify-center cursor-pointer">
