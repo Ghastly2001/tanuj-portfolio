61421e09d0193a6433b214ad4d282e6f3efb42c8
Divyansh <divyanshverma18may@gmail.com>
2025-03-29 15:29:39 +0530

    fix: responsive-bring-section

--- src/components/bring-section.tsx
+++ src/components/bring-section.tsx
@@ -3,19 +3,19 @@ import React from "react";
 
 const BringSection = () => {
   return (
-    <div className="max-w-5xl mx-auto mb-[100px] px-4 sm:px-10 flex flex-col gap-5">
+    <div className="max-w-5xl mx-auto mb-[100px] px-8 sm:px-10 flex flex-col gap-5">
       <div className="flex flex-col items-start mt-[110px]">
-        <h1 className="w-full text-3xl font-bold leading-[32px] text-center">
+        <h1 className="w-full text-2xl  lg:text-3xl font-bold leading-[32px] text-start lg:text-center">
           What I can <span className="text-[#4BB543]">bring to the table</span>{" "}
           as an Employee
         </h1>
       </div>
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-[26px] mt-[40px]">
+      <div className="grid grid-cols-2 lg:grid-cols-3 gap-[26px] mt-[40px]">
         <div className=" border border-[#EBEBEB] rounded-xl  flex flex-col gap-[30px] p-4">
           <div className="relative flex items-center justify-center h-[150px] w-full -z-10">
             <Image src={"/clock.svg"} fill alt="clock" />
           </div>
-          <p className="text-base text-[#333333] text-center">
+          <p className="text-sm lg:text-base text-[#333333] text-center">
             Conducting <span className="font-bold">research</span> to understand
             the needs, behaviors of users.
           </p>
@@ -24,7 +24,7 @@ const BringSection = () => {
           <div className="relative flex items-center justify-center h-[150px] w-full -z-10">
             <Image src={"/flag.svg"} fill alt="clock" />
           </div>
-          <p className="text-base text-[#333333] text-center">
+          <p className="text-sm lg:text-base text-[#333333] text-center">
             Rough <span className="font-bold"> sketches/ideations </span> or{" "}
             <span className="font-bold">low- fidelity</span> mockups of your
             initial ideas.
@@ -34,7 +34,7 @@ const BringSection = () => {
           <div className="relative flex items-center justify-center h-[150px] w-full -z-10">
             <Image src={"/notes.svg"} fill alt="clock" />
           </div>
-          <p className="text-base text-[#333333] text-center">
+          <p className="text-sm lg:text-base text-[#333333] text-center">
             Creating detailed <span className="font-bold">documentation</span>{" "}
             and <span className="font-bold">specs</span> of designs.
           </p>
@@ -43,7 +43,7 @@ const BringSection = () => {
           <div className="relative flex items-center justify-center h-[150px] w-full -z-10">
             <Image src={"/photos.svg"} fill alt="clock" />
           </div>
-          <p className="text-base text-[#333333] text-center">
+          <p className="text-sm lg:text-base text-[#333333] text-center">
             <span className="font-bold">High-fidelity</span> and visual designs,
             include layout, guide & graphic.
           </p>
@@ -52,7 +52,7 @@ const BringSection = () => {
           <div className="relative flex items-center justify-center h-[150px] w-full -z-10">
             <Image src={"/flower-pot.svg"} fill alt="clock" />
           </div>
-          <p className="text-base text-[#333333] text-center">
+          <p className="text-sm lg:text-base text-[#333333] text-center">
             Testing designs with real users for{" "}
             <span className="font-bold">usability issues</span> or areas for
             improve
@@ -62,7 +62,7 @@ const BringSection = () => {
           <div className="relative flex items-center justify-center h-[150px] w-full -z-10">
             <Image src={"/plane.svg"} fill alt="clock" />
           </div>
-          <p className="text-base text-[#333333] text-center">
+          <p className="text-sm lg:text-base text-[#333333] text-center">
             Creating a cohesive <span className="font-bold">design system</span>{" "}
             that includes design principles, comps.
           </p>
--- src/components/expertise-section.tsx
+++ src/components/expertise-section.tsx
@@ -3,66 +3,81 @@ import React from "react";
 
 const ExpertiseSection = () => {
   return (
-    <div className="bg-[#F7F7FA] h-[700px] w-full max-sm:px-4 flex flex-col items-center justify-center ">
+    <div className="bg-[#F7F7FA] h-full lg:h-[700px] w-full max-sm:px-4 max-sm:py-20  flex flex-col items-center justify-center">
       <div className="max-w-5xl mx-auto px-4 sm:px-10">
-        <div className="relative w-full flex h-[300px] items-center">
+        <div className="relative w-full flex h-[300px] items-start lg:items-center">
           <div className="w-full mb-auto flex flex-col gap-5">
-            <h1 className="font-bold  text-2xl lg:text-3xl w-full">
+            <h1 className="font-bold text-xl lg:text-3xl w-full hidden lg:block">
               Skilled in industry-leading software,
               <br /> I have <span className="text-[#4BB543]">expertise</span> in
               these tools.
             </h1>
-            <p className="text-sm text-[#333333] w-full tracking-wider leading-[28px]">
+            <h1 className="font-bold text-xl lg:text-3xl w-full lg:hidden">
+              Skilled in industry-leading software, I have{" "}
+              <span className="text-[#4BB543]">expertise</span> in these tools.
+            </h1>
+
+            <p className="text-sm text-[#333333] w-full tracking-wider leading-[28px] ">
               From creating wireframes and prototypes to high-fidelity mockups
               and animations, I blend design tools and fashion insights to bring
               my creations to life.
             </p>
           </div>
-          <div className="lg:block relative w-[500px] h-[400px] mb-24 hidden ">
+
+          <div className="relative w-[500px] h-[400px] mb-24  hidden lg:block">
+            <Image src={"/person-thinking.svg"} fill alt="expertise" />
+          </div>
+          <div className=" w-[250px] h-[200px] relative  lg:hidden -top-16 ">
             <Image src={"/person-thinking.svg"} fill alt="expertise" />
           </div>
         </div>
-        <div className="w-full flex items-center justify-between lg:gap-10">
-          <div className="flex flex-col gap-4">
+        <div className="w-full flex flex-col lg:flex-row items-start lg:items-center justify-between lg:gap-10 gap-8">
+          <div className="flex flex-row lg:flex-col gap-4">
             <Image
               src={"/figma.svg"}
               width={36}
               height={36}
               alt="figma"
-              className="h-1/2"
+              className="h-1/2 hidden lg:block"
             />
-            <h1 className="font-bold text-xl">Figma</h1>
-            <p className="text-black/80">
-              3+ years of experience with Figma, I&apos;m now pretty much a pro
-              (or close enough)!
-            </p>
+            <div className="flex flex-col gap-2 lg:gap-4">
+              <h1 className="font-bold text-xl">Figma</h1>
+              <p className="text-black/80 leading-[28px] text-sm ">
+                3+ years of experience with Figma, I&apos;m now pretty much a
+                pro (or close enough)!
+              </p>
+            </div>
           </div>
-          <div className="flex flex-col gap-4">
+          <div className="flex flex-col gap-2 lg:gap-4">
             <Image
               src={"/adobe-suite.svg"}
               width={64}
               height={64}
               alt="figma"
-              className="h-1/2"
+              className="h-1/2 hidden lg:block"
             />
-            <h1 className="font-bold text-xl">Adobe Suite</h1>
-            <p className="text-black/80">
-              Using adobe suit for last 3+ years. Fav: Illustrator, Photoshop,
-              InDesign, XD.
-            </p>
+            <div className="flex flex-col gap-4">
+              <h1 className="font-bold text-xl">Adobe Suite</h1>
+              <p className="text-black/80 leading-[28px] text-sm ">
+                Using adobe suit for last 3+ years. Fav: Illustrator, Photoshop,
+                InDesign, XD.
+              </p>
+            </div>
           </div>
-          <div className="flex flex-col gap-4">
+          <div className="flex flex-col gap-2 lg:gap-4">
             <Image
               src={"/miro.svg"}
               width={60}
               height={60}
               alt="figma"
-              className="h-1/2"
+              className="h-1/2 hidden lg:block"
             />
-            <h1 className="font-bold text-xl">Miro</h1>
-            <p className="text-black/80">
-              I am an expert in using Miro. Knowledge of JIRA as well.
-            </p>
+            <div className="flex flex-col gap-2 lg:gap-4">
+              <h1 className="font-bold text-xl">Miro</h1>
+              <p className="text-black/80 leading-[28px] text-sm ">
+                I am an expert in using Miro. Knowledge of JIRA as well.
+              </p>
+            </div>
           </div>
         </div>
       </div>
